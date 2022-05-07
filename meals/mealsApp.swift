f1c93034ba61086864c713530548112f1bfad3a8
Alex Chaplianka <alexettelis@gmail.com>
2022-05-07 14:44:57 +0300

    Initial Commit

---
+++ meals/mealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mealsApp.swift
+//  meals
+//
+//  Created by aclowkey on 07/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct mealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
