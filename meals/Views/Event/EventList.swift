70bd129a695e9676206fede14f2fea70ca8f4498
Alex Chaplianka <alexettelis@gmail.com>
2022-10-09 10:26:35 +0300

    Photo playgrounds

--- meals/Views/Event/EventList.swift
+++ meals/Views/Event/EventList.swift
@@ -15,21 +15,21 @@ struct EventList: View {
     @EnvironmentObject var eventStore: EventStore
     @EnvironmentObject var photoStore: PhotoStore
     @State var ready = false
-
+    
     // Meal event logging
     @State var showMealSelector = false
     @State var showLogEventAlert = false
     @State var mealToLog: Meal? = nil
     @State var mealEventDate: Date = Date()
-
+    
     // Meal event logging alerts
     @State var showMealSuccessAlert = false
-
+    
     // Event statistics
     @State var selectedEvent: Event? = nil
     @State var hours = 3
     let hourOptions = [3, 6]
-
+    
     var body: some View {
         VStack {
             if let selectedEvent = selectedEvent {
@@ -40,43 +40,48 @@ struct EventList: View {
                 EventListSkeleton()
             }
         }
-                .background(.gray.opacity(0.2))
-                .onAppear {
-                    withAnimation {
-                        ready = true
-                        selectedEvent = eventStore.events.first
-                    }
-                }
+        .background(.gray.opacity(0.2))
+        .onAppear {
+            withAnimation {
+                ready = true
+                selectedEvent = eventStore.events.first
+            }
+        }
     }
-
+    
     var header: some View {
         let colors = [Color(hex: 0x424242), Color(hex: 0x002266)]
         let meal = selectedEvent != nil
-                ? mealStore.getMeal(event: selectedEvent!)
-                : nil
+        ? mealStore.getMeal(event: selectedEvent!)
+        : nil
         let image = meal != nil
-                ? try? photoStore.loadImage(mealID: meal!.id)
-                : nil
-
+        ? try? photoStore.loadImage(mealID: meal!.id)
+        : nil
+        
         return HStack {
             if let selectedEvent = selectedEvent, let meal = meal {
                 // Todo: Maybe this should be it's own component "MealHeader" ?
                 ZStack {
                     // Photo
-                    HStack {
-                        if let image = image {
-                            Image(uiImage: image).resizable().scaledToFill()
-                        } else {
-                            LinearGradient(colors: colors, startPoint: .leading, endPoint: .trailing)
+                    GeometryReader{ geo in
+                        HStack {
+                            if let image = image {
+                                Image(uiImage: image).resizable()
+                            } else {
+                                LinearGradient(colors: colors, startPoint: .leading, endPoint: .trailing)
+                            }
                         }
+                        .scaledToFill()
+                        .frame(width: geo.size.width, height: geo.size.height)
+                        .clipped()
                     }
-                            .frame(height: 120)
+                    
                     VStack(alignment: .trailing) {
                         Spacer()
                         HStack {
                             Text(meal.name)
-                                    .font(.largeTitle)
-                                    .minimumScaleFactor(0.01)
+                                .font(.largeTitle)
+                                .minimumScaleFactor(0.01)
                             Spacer()
                         }
                         HStack {
@@ -85,43 +90,46 @@ struct EventList: View {
                         }
                         Spacer()
                     }
-                            .padding()
-                            .foregroundColor(.white)
-                            .background(
-                                    .linearGradient(
-                                            colors: [.black, .black.opacity(0)],
-                                            startPoint: .bottom,
-                                            endPoint: .top)
-                            )
-                            .padding([.leading, .trailing], 5)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(
+                        .linearGradient(
+                            stops:[
+                                Gradient.Stop(color: .black, location: 0),
+                                Gradient.Stop(color: .black.opacity(0), location: 0.9)
+                            ],
+                            startPoint: .bottom,
+                            endPoint: .top)
+                    )
+                    .padding([.leading, .trailing], 5)
                 }
-
+                
             } else {
                 ZStack {
                     LinearGradient(colors: colors, startPoint: .leading, endPoint: .trailing)
                     HStack {
                         VStack(alignment: .leading) {
                             Text("No event is selected")
-                                    .font(.headline)
-                                    .frame(height: 30)
+                                .font(.headline)
+                                .frame(height: 30)
                             Text("Select an event in the timeline")
-                                    .font(.caption)
-                                    .frame(height: 10)
+                                .font(.caption)
+                                .frame(height: 10)
                         }
                         Spacer()
                     }
-                            .padding()
+                    .padding()
                 }
             }
         }
-                .frame(height: 100)
+        .frame(height: 150)
     }
-
+    
     func statistics(event: Event) -> some View {
         VStack {
             HStack {
                 Text("Statistics")
-                        .font(.headline)
+                    .font(.headline)
                 Spacer()
                 Picker("Hours", selection: $hours) {
                     ForEach(hourOptions, id: \.self) { hour in
@@ -129,7 +137,7 @@ struct EventList: View {
                     }
                 }
             }
-                    .padding([.leading, .top, .trailing], 10)
+            .padding([.leading, .top, .trailing], 10)
             VStack {
                 Text("Glucose")
                 MetricGraph(metricStore: metricStore, event: event, dataType: .Glucose, hours: hours)
@@ -140,21 +148,21 @@ struct EventList: View {
             }
             Spacer()
         }
-                .background(Color(uiColor: .systemBackground))
-                .cornerRadius(15)
-                .padding([.leading, .trailing, .bottom], 5)
+        .background(Color(uiColor: .systemBackground))
+        .cornerRadius(15)
+        .padding([.leading, .trailing, .bottom], 5)
     }
-
+    
     var timeline: some View {
         let timelineEvents = getTimelineEvents()
         return VStack {
             HStack {
                 Text("Timeline")
-                        .font(.headline)
+                    .font(.headline)
                 Spacer()
             }
-                    .padding([.leading, .top, .trailing], 10)
-
+            .padding([.leading, .top, .trailing], 10)
+            
             HStack {
                 if eventStore.events.isEmpty {
                     noData
@@ -163,31 +171,31 @@ struct EventList: View {
                         HStack {
                             ForEach(Array(timelineEvents.enumerated()), id: \.1.hashValue) { index, timelineEvent in
                                 timelineCard(
-                                        meal: mealStore.getMeal(event: timelineEvent.event)!,
-                                        event: timelineEvent.event,
-                                        firstInDay:
+                                    meal: mealStore.getMeal(event: timelineEvent.event)!,
+                                    event: timelineEvent.event,
+                                    firstInDay:
                                         index == 0 || !isSameDay(
-                                                date1: timelineEvents[index - 1].event.date,
-                                                date2: timelineEvent.event.date
+                                            date1: timelineEvents[index - 1].event.date,
+                                            date2: timelineEvent.event.date
                                         )
                                 )
-                                        .onTapGesture {
-                                            withAnimation {
-                                                selectedEvent = timelineEvent.event
-                                            }
-                                        }
+                                .onTapGesture {
+                                    withAnimation {
+                                        selectedEvent = timelineEvent.event
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
-                    .padding(.bottom, 10)
+            .padding(.bottom, 10)
         }
-                .background(Color(uiColor: .systemBackground))
-                .cornerRadius(15)
-                .padding([.leading, .trailing], 5)
+        .background(Color(uiColor: .systemBackground))
+        .cornerRadius(15)
+        .padding([.leading, .trailing], 5)
     }
-
+    
     func timelineCard(meal: Meal, event: Event, firstInDay: Bool) -> some View {
         VStack {
             HStack {
@@ -198,60 +206,60 @@ struct EventList: View {
             }
             HStack {
                 Text(DateUtils.formatTime(date: event.date))
-                        .font(.caption)
-                        .padding(0)
+                    .font(.caption)
+                    .padding(0)
                 Spacer()
             }
             MealCard(
-                    font: .caption,
-                    meal: meal,
-                    image: try? photoStore.loadImage(mealID: meal.id)
+                font: .caption,
+                meal: meal,
+                image: try? photoStore.loadImage(mealID: meal.id)
             )
-                    .clipShape(
-                            RoundedRectangle(
-                                    cornerSize: CGSize(width: 10, height: 10)))
-                    .frame(width: 100, height: 100)
+            .clipShape(
+                RoundedRectangle(
+                    cornerSize: CGSize(width: 10, height: 10)))
+            .frame(width: 100, height: 100)
         }
-                .padding(.leading, 10)
+        .padding(.leading, 10)
     }
-
+    
     var noData: some View {
         HStack(alignment: .center) {
             Spacer()
             VStack(alignment: .center) {
                 Image(systemName: "tray.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(.secondary.opacity(0.5))
-                        .font(.system(size: 30, weight: .ultraLight))
-                        .frame(width: 80)
-
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.secondary.opacity(0.5))
+                    .font(.system(size: 30, weight: .ultraLight))
+                    .frame(width: 80)
+                
                 Text("No data")
-                        .font(.title)
-
+                    .font(.title)
+                
                 HStack(alignment: .center) {
                     Spacer()
                     Text("Log an event")
-                            .font(.body)
+                        .font(.body)
                     Spacer()
                 }
             }
             Spacer()
         }
-
+        
     }
-
+    
     // Event handler
     func onMealTap(meal: Meal) {
         showLogEventAlert = true
         mealToLog = meal
     }
-
+    
     // Helpers
     func isSameDay(date1: Date, date2: Date) -> Bool {
         Calendar.current.isDate(date1, equalTo: date2, toGranularity: .day)
     }
-
+    
     func getTimelineEvents() -> [TimelineEvent] {
         eventStore.events.compactMap {
             // This really shouldn't happen...
@@ -260,12 +268,12 @@ struct EventList: View {
                 return nil
             }
             return TimelineEvent(
-                    event: $0,
-                    mealUpdatedAt: meal.updatedAt
+                event: $0,
+                mealUpdatedAt: meal.updatedAt
             )
         }
     }
-
+    
 }
 
 struct TimelineEvent: Hashable {
@@ -274,29 +282,29 @@ struct TimelineEvent: Hashable {
 }
 
 struct EventList_Previews: PreviewProvider {
-
+    
     static var previews: some View {
         let mealID = 1
         let store = Store(
-                meals: [
-                    Meal(id: mealID, name: "Test", description: "Test")
-                ],
-                events: [
-                    Event(meal_id: mealID, id: 1),
-                    Event(meal_id: mealID),
-                    Event(meal_id: mealID),
-                    Event(meal_id: mealID),
-                    Event(meal_id: mealID),
-                    Event(meal_id: mealID),
-                    Event(meal_id: mealID),
-                    Event(meal_id: mealID),
-                    Event(meal_id: mealID),
-                ],
-                settings: Settings(dataSourceType: .Debug)
+            meals: [
+                Meal(id: mealID, name: "Test", description: "Test")
+            ],
+            events: [
+                Event(meal_id: mealID, id: 1),
+                Event(meal_id: mealID),
+                Event(meal_id: mealID),
+                Event(meal_id: mealID),
+                Event(meal_id: mealID),
+                Event(meal_id: mealID),
+                Event(meal_id: mealID),
+                Event(meal_id: mealID),
+                Event(meal_id: mealID),
+            ],
+            settings: Settings(dataSourceType: .Debug)
         )
         EventList(metricStore: store.metricStore)
-                .environmentObject(store.mealStore)
-                .environmentObject(store.eventStore)
-                .environmentObject(store.photoStore)
+            .environmentObject(store.mealStore)
+            .environmentObject(store.eventStore)
+            .environmentObject(store.photoStore)
     }
 }
